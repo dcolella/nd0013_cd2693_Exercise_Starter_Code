baae87f0095fb16cb862e17c82f6d8ba97e87c84
Donato Colella <donato.colella@stellantis.com>
2025-11-11 16:24:14 +0100

    fixed ICP function.

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -102,32 +102,38 @@ void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::
 
 Eigen::Matrix4d ICP(PointCloudT::Ptr target, PointCloudT::Ptr source, Pose startingPose, int iterations){
 
-    Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
+	// Defining a rotation matrix and translation vector
+  	Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
 
-    // align source with starting pose
-    Eigen::Matrix4d initTransform = transform2D(startingPose.theta, startingPose.position.x, startingPose.position.y);
-    PointCloudT::Ptr transformSource (new PointCloudT); 
-    pcl::transformPointCloud (*source,* transformSource, initTransform);
+  	// align source with starting pose
+  	Eigen::Matrix4d initTransform = transform3D(startingPose.rotation.yaw, startingPose.rotation.pitch, startingPose.rotation.roll, startingPose.position.x, startingPose.position.y, startingPose.position.z);
+  	PointCloudT::Ptr transformSource (new PointCloudT); 
+  	pcl::transformPointCloud (*source, *transformSource, initTransform);
 
-    pcl::console::TicToc time;
-    time.tic ();
-    pcl::IterativeClosestPoint<PointT, PointT> icp;
-	icp.setMaximumIterations (iterations);
-	icp.setInputSource (transformSource);
-	icp.setInputTarget (target);
-	PointCloudT::Ptr cloud_icp (new PointCloudT);  // ICP output point cloud
-	icp.align (*cloud_icp);
+  		
+	pcl::console::TicToc time;
+  	time.tic ();
+  	pcl::IterativeClosestPoint<PointT, PointT> icp;
+  	icp.setMaximumIterations (iterations);
+  	icp.setInputSource (transformSource);
+  	icp.setInputTarget (target);
+	icp.setMaxCorrespondenceDistance (2);
+	
 
-	if (icp.hasConverged ())
-	{
-		std::cout << "\nICP has converged, score is " << icp.getFitnessScore () << std::endl;
-		transformation_matrix = icp.getFinalTransformation ().cast<double>();
-		transformation_matrix =  transformation_matrix * initTransform;
-		return transformation_matrix;
-	}
-	cout << "WARNING: ICP did not converge" << endl;
+  	PointCloudT::Ptr cloud_icp (new PointCloudT);  // ICP output point cloud
+  	icp.align (*cloud_icp);
+  	
 
-    return transformation_matrix;
+  	if (icp.hasConverged ())
+  	{
+  		transformation_matrix = icp.getFinalTransformation ().cast<double>();
+  		transformation_matrix =  transformation_matrix * initTransform;
+
+  		return transformation_matrix;
+  	}
+	else
+  		cout << "WARNING: ICP did not converge" << endl;
+  	return transformation_matrix;
 
 }
 
