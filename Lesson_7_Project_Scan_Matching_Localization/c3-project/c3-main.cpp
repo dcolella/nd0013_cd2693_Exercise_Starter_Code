1cc8282fdac7705f0b91a9a862e6ccdc0119431d
Donato Colella <donato.colella@stellantis.com>
2025-11-17 11:06:58 +0100

    .

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -367,8 +367,11 @@ int main(){
 		double vehicle_speed = getVehicleSpeedMs(vehicle);
 		double vehicle_yaw_rate = getVehicleYawRateRadS(vehicle);
 
+		std::cout << "speed:" << vehicle_speed << endl;
+		std::cout << "yaw rate:" << vehicle_yaw_rate << endl;
+
 		printPosePosition(truePose, "truePose.");
-		
+
 		pose = predictPoseFromSpeedAndYawRate(pose, vehicle_speed, vehicle_yaw_rate, pose_time);
 
 		printTime(pose_time, "pose_time");
