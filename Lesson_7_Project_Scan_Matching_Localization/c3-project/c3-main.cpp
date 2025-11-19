e3586891fa581f8a90bf76da7e322222d668a95f
Donato Colella <donato.colella@stellantis.com>
2025-11-19 19:03:22 +0100

    .

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -360,7 +360,9 @@ int main(){
 			Eigen::Matrix4d transform = transform3D(pose.rotation.yaw, pose.rotation.pitch, pose.rotation.roll, pose.position.x, pose.position.y, pose.position.z);
 			// TODO: Find pose transform by using ICP or NDT matching
 			//pose = ....
-			transform = getTransformWithICP(mapCloud, cloudFiltered, transform, 50); 
+			
+			
+			transform = getTransformWithICP(mapCloud, cloudFiltered, getTransformWithNDT(mapCloud, cloudFiltered, transform, 50), 50);
 			pose = getPose(transform);
 
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
