0616b2b1898eaef0ed38960b5ae455022bec6ec9
Donato Colella <donato.colella@stellantis.com>
2025-11-21 18:11:06 +0100

    .

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -473,7 +473,7 @@ int main(){
 				
 				pose = predictPoseFromSpeedAndYawRate(pose, vehicle_speed, vehicle_yaw_rate, lastPredictionTime);
 				transform = transform3D(pose.rotation.yaw, pose.rotation.pitch, pose.rotation.roll, pose.position.x, pose.position.y, pose.position.z);
-				transform = getTransformWithNDT(mapCloud, cloudFiltered, transform, 30, logger);
+				//transform = getTransformWithNDT(mapCloud, cloudFiltered, transform, 30, logger);
 				transform = getTransformWithICP(mapCloud, cloudFiltered, getTransformWithNDT(mapCloud, cloudFiltered, transform, 50, logger), 50, logger);
 				
 				pose = getPose(transform);
