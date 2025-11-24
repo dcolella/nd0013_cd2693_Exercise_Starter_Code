a0a14c9e99be60c3c3ce2233cedc3c049798d89d
Donato Colella <donato.colella@stellantis.com>
2025-11-24 13:23:15 +0100

    .

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -539,11 +539,12 @@ int main(){
 				if (lastCloudFiltered->empty()) {
 					transform = getTransformWithICP(mapCloud, cloudFiltered, transform, 50, logger);
 					*lastCloudFiltered = *cloudFiltered;
+					pcl::copyPointCloud(*cloudFiltered, *lastCloudFiltered);
 					std::cout << "lastCloudFiltered empty, using mapCloud for matching." << std::endl;
 
 				}else{
 					transform = getTransformWithICP(lastCloudFiltered, cloudFiltered, transform, 50, logger);
-					*lastCloudFiltered = *cloudFiltered;
+					pcl::copyPointCloud(*cloudFiltered, *lastCloudFiltered);
 					std::cout << "Using lastCloudFiltered scan for matching." << std::endl;
 
 				}
